d8b9770103cbd14d987be93ef7591b3dc90989a7
Jessica Doe <jessica@brickeye.com>
2024-03-06 13:50:19 -0800

    updating batch paths

--- tablet.bat
+++ tablet.bat
@@ -2,4 +2,4 @@
 
 echo Hello World!
 ::"C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\Scripts\activate.bat" "C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\" & streamlit run "C:\Users\Jessica Doe\Documents\Python\tablet-dev\app\app.py" --server.port 8501
-"C:\tablet-dev.venv\Scripts\activate.bat" "C:\tablet-dev.venv\" & streamlit run "C:\tablet-dev\app\app.py" --server.port 8501
+"C:\Users\jessi\tablet-dev.venv\Scripts\activate.bat" "C:\Users\jessi\tablet-dev.venv\" & streamlit run "C:\Users\jessi\tablet-dev\app\app.py" --server.port 8501
