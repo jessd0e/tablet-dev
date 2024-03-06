4dd2c5c12087c0a18d0aef5d9b83cc97da7368d0
Jessica Doe <jessica@brickeye.com>
2024-03-06 13:30:31 -0800

    adding batch file

--- app/app.py
+++ app/app.py
@@ -2,14 +2,16 @@ import streamlit as st
 import serial.tools.list_ports as list_ports
 
 
-# docker build . -t tablet-dev --no-cache=true
-# docker run -p 8501:8501 --restart always tablet-dev
+# docker build . -t jessicadoe/tablet-dev --no-cache=true
+# docker run -p 8501:8501 --restart always jessicadoe/tablet-dev
 
 
 button = st.button("Press for Ports")
 if button:
     st.write("Hello World!")
     ports = list_ports.comports()
+    st.write("Ports")
+    st.write(ports)
 
     for port, desc, hwid in sorted(ports):
         st.write(f"{port}: {desc} [{hwid}]")
---
+++ tablet.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+echo Hello World!
+"C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\Scripts\activate.bat" "C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\" & streamlit run "C:\Users\Jessica Doe\Documents\Python\tablet-dev\app\app.py" --server.port 8501
