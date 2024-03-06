4613d35c83b81af51512fa724f28e4e65eed2b10
Jessica Doe <jessica@brickeye.com>
2024-03-06 14:17:54 -0800

    another tablet bat

--- tablet.bat
+++ tablet.bat
@@ -2,4 +2,5 @@
 
 echo Hello World!
 ::"C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\Scripts\activate.bat" "C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\" & streamlit run "C:\Users\Jessica Doe\Documents\Python\tablet-dev\app\app.py" --server.port 8501
-"C:\Users\jessi\tablet-dev.venv\Scripts\activate.bat" "C:\Users\jessi\tablet-dev.venv\" & streamlit run "C:\Users\jessi\tablet-dev\app\app.py" --server.port 8501
+"C:\Users\jessi\tablet-dev.venv\Scripts\activate"
+streamlit run "C:\Users\jessi\tablet-dev\app\app.py" --server.port 8501
