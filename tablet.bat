7f5a8c1d9605b37eb00f22b259d3116002f5d509
Jessica Doe <jessica@brickeye.com>
2024-03-06 13:32:37 -0800

    updating bat file for tablet use

--- tablet.bat
+++ tablet.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 
 echo Hello World!
-"C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\Scripts\activate.bat" "C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\" & streamlit run "C:\Users\Jessica Doe\Documents\Python\tablet-dev\app\app.py" --server.port 8501
+::"C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\Scripts\activate.bat" "C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\" & streamlit run "C:\Users\Jessica Doe\Documents\Python\tablet-dev\app\app.py" --server.port 8501
+"C:\tablet-dev.venv\Scripts\activate.bat" "C:\tablet-dev.venv\" & streamlit run "C:\tablet-dev\app\app.py" --server.port 8501
