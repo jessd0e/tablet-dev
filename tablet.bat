49beaca2f07faa89f2dbaae361db1ce6d00d402f
Jessica Doe <jessica@brickeye.com>
2024-03-06 14:25:45 -0800

    new streamlit run command in bat

--- tablet.bat
+++ tablet.bat
@@ -2,5 +2,4 @@
 
 echo Hello World!
 ::"C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\Scripts\activate.bat" "C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\" & streamlit run "C:\Users\Jessica Doe\Documents\Python\tablet-dev\app\app.py" --server.port 8501
-"C:\Users\jessi\tablet-dev.venv\Scripts\activate"
-streamlit run "C:\Users\jessi\tablet-dev\app\app.py" --server.port 8501
+"C:\Users\jessi\tablet-dev.venv\Scripts\streamlit" run "C:\Users\jessi\tablet-dev\app\app.py" --server.port 8501
