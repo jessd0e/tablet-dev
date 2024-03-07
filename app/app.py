2b92fb4d7f3370641aeaeb88a6a7eb45b4c69b50
Jessica Doe <jessica@brickeye.com>
2024-03-06 19:54:23 -0800

    page change

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import serial.tools.list_ports as list_ports
 # docker build . -t jessicadoe/tablet-dev --no-cache=true
 # docker run -p 8501:8501 --restart always jessicadoe/tablet-dev
 
-
+st.title("Welcome to Brickeye's Tablet!")
 button = st.button("Press for Ports")
 if button:
     st.write("Hello World!")
