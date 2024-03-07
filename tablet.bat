aac1ea8e8b750e384086d7599011d68b7c982097
Jessica Doe <jessica@brickeye.com>
2024-03-06 16:02:01 -0800

    adding headless server

--- tablet.bat
+++ tablet.bat
@@ -3,4 +3,4 @@
 echo Hello World!
 ::"C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\Scripts\activate.bat" "C:\Users\Jessica Doe\Documents\Python\tablet-dev.venv\" & streamlit run "C:\Users\Jessica Doe\Documents\Python\tablet-dev\app\app.py" --server.port 8501
 call "C:\Users\jessi\tablet-dev\venv\Scripts\activate.bat"
-streamlit run "C:\Users\jessi\tablet-dev\app\app.py" --server.port 8501
+streamlit run "C:\Users\jessi\tablet-dev\app\app.py" --server.headless true --server.port 8501
